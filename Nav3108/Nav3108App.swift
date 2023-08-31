af48b4b5d1dc8834ba0b0af1d96a8908f88f829b
KoivIgor <ipkoiv@vtb.ru>
2023-08-31 20:41:46 +0300

    Initial Commit

---
+++ Nav3108/Nav3108App.swift
@@ -0,0 +1,17 @@
+//
+//  Nav3108App.swift
+//  Nav3108
+//
+//  Created by Koiv Igor on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Nav3108App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
